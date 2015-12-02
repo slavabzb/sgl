c4846fe0ccf83e9c70da4c2c166f454bd0369c15
Vyacheslav Bezborodov <v.bezborodov@pressfoto.ru>
2015-12-02 09:05:46 +0500

    Refactoring

--- include/sgl/view/edge_list.h
+++ include/sgl/view/edge_list.h
@@ -21,6 +21,29 @@
  * @endcond
  */
 
+/**
+ * @cond
+ *
+ * Copyright (c) 2015-2016
+ *
+ * Bezborodov, V.A. vyacheslav.bezborodov@gmail.com
+ * Golodov, V.A. avaksa@gmail.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * @endcond
+ */
+
 #pragma once
 
 #include <map>
@@ -38,7 +61,7 @@ namespace sgl
     {
         /**
          * @ref edge_list
-         * @ingropu view
+         * @ingroup view
          */
         class edge_list: public view
         {
@@ -47,25 +70,25 @@ namespace sgl
         #endif
 
             using base_t = view;
-            
+
         public:
             /**
              * Constructor.
              * Constructs an edge_list using given properties.
-             * 
+             *
              * @param oriented - a flag; true, if graph is oriented, false otherwise,
              * @param weighted - a flag; true, if graph is weighted, false otherwise.
              */
             edge_list(bool oriented = false, bool weighted = false);
-            
+
             /**
              * Copy constructor.
              * Constructs an edge_list using other as pattern.
-             * 
+             *
              * @param other - a view to be constructed from.
              */
             edge_list(const view& other);
-            
+
             virtual ~edge_list();
 
             virtual void add_node(const sgl::core::node& node) override;
@@ -83,16 +106,16 @@ namespace sgl
 
             /**
              * Assigns rhs to the edge_list.
-             * 
+             *
              * @param rhs - an edge_list to be assigned with.
              * @return The edge_list.
              * @throw std::invalid_argument if oriented/weighted flags are mismatched.
              */
             edge_list& operator=(const edge_list& rhs);
-            
+
             /**
              * Assigns ths to the edge_list.
-             * 
+             *
              * @param rhs - an edge_list to be assigned with.
              * @return The edge_list.
              * @throw std::invalid_argument if oriented/weighted flags are mismatched.
